2521c31e80737703fc2f29667cbd99068e1db9fc
Alaa Alaff <travel98_alaff@hotmail.com>
2021-08-16 03:48:38 +0300

    make postMaker modal responsive full styled

--- development/js/script.js
+++ development/js/script.js
@@ -1,3 +1,4 @@
+// Changing hightlights on pages navbar
 const headerPage = document.querySelectorAll(".page");
 
 headerPage.forEach((page) =>
@@ -6,3 +7,46 @@ headerPage.forEach((page) =>
     page.classList.toggle("active");
   })
 );
+
+// Handling Post share modal
+const postMaker = document.querySelector(".postMaker-container");
+
+const sharePost = document.querySelector(".share__input");
+
+const closeModalBtn = document.querySelector(".close-modal");
+
+const overlay = document.querySelector(".overlay");
+
+const body = document.querySelector("*");
+
+const openModal = () => {
+  postMaker.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+  body.style.overflow = "hidden";
+  window.scrollTo(0, 0);
+};
+
+const closeModal = () => {
+  postMaker.classList.add("hidden");
+  overlay.classList.add("hidden");
+  body.style.overflow = "scroll";
+};
+
+sharePost.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+// Enable post btn while writing post
+const postInput = document.querySelector(".details-box__description");
+
+const postShare = document.querySelector(".post-btn");
+
+postInput.addEventListener("keyup", (e) => {
+  console.log(postInput.value);
+  if (postInput.value === "") {
+    postShare.style.backgroundColor = "#e4e6eb";
+  } else {
+    console.log(false);
+    postShare.style.backgroundColor = "#1876f2";
+  }
+});
